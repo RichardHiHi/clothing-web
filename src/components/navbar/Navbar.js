1afb10524b4a7211f4334d7d8a79c0c90487ad5f
thanh <thanhtrong.nguyen1602@gmail.com>
2021-03-31 12:09:57 +0700

    change pc

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,56 +10,49 @@ const Navbar = () => {
   const { miniAction } = useButtonContext();
   return (
     <div className='nav'>
-      <Grid container alignItems='center'>
-        <Grid container item lg={3} md={9} alignItems='center' xs={9}>
-          <Grid item md={6} lg={0} xs={6}>
-            <button
-              className='nav-toggle'
-              onClick={() => miniAction('open', 'SideBar')}
-            >
-              <ReorderIcon style={{ fontSize: 30 }} />
-            </button>
-          </Grid>
-          <Grid item md={6} lg={12} xs={6}>
-            <a href='#' className='logo'>
-              <img src={logo} alt='' />
-            </a>
-          </Grid>
-        </Grid>
-        <Grid item lg={6}>
-          <ul className='nav-links'>
-            <li>
-              <a href='#'>Home</a>
-            </li>
-            <li>
-              <a href='#'>Products</a>
-              <ul className='sub-menu'>
-                <li>
-                  <a href='#'>quần áo</a>
-                </li>
-                <li>
-                  <a href='#'>giày</a>
-                </li>
-                <li>
-                  <a href='#'>nhẫn</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href='#'>Sale</a>
-            </li>
-            <li>
-              <a href='#'>About</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
-        </Grid>
-        <Grid item lg={3} md={3} xs={3}>
-          <NavButton />
-        </Grid>
-      </Grid>
+      <div className='navbar'>
+        <div className='logo-wrapper'>
+          <button
+            className='nav-toggle-btn'
+            onClick={() => miniAction('open', 'SideBar')}
+          >
+            <ReorderIcon style={{ fontSize: 30 }} />
+          </button>
+
+          <a href='#' className='logo'>
+            <img src={logo} alt='' />
+          </a>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>Products</a>
+            <ul className='sub-menu'>
+              <li>
+                <a href='#'>quần áo</a>
+              </li>
+              <li>
+                <a href='#'>giày</a>
+              </li>
+              <li>
+                <a href='#'>nhẫn</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href='#'>Sale</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Blog</a>
+          </li>
+        </ul>
+        <NavButton />
+      </div>
     </div>
   );
 };
