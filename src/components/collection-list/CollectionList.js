ce2ab5e3de8b8de84383d4fc4fec7249dc0dbf63
thanh <thanhtrong.nguyen1602@gmail.com>
2021-03-31 20:54:19 +0700

    change pc

--- src/components/collection-list/CollectionList.js
+++ src/components/collection-list/CollectionList.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './collectionList.scss';
 import Grid from '@material-ui/core/Grid';
+import bag from '../../assets/collect/bag.jpg';
+import shoes from '../../assets/collect/shoes.jpeg';
+import watch from '../../assets/collect/watch.jpeg';
+import women from '../../assets/collect/women.jpg';
+import women_1 from '../../assets/collect/women_1.jpg';
 
 const CollectionList = () => {
   return (
@@ -8,14 +13,50 @@ const CollectionList = () => {
       <div className='collection-list-container'>
         <div className='collection-list'>
           <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              <h2>1</h2>
+            <Grid item md={12} lg={6}>
+              <div className='item-collection-content'>
+                {/* <a href='#'>
+                  <img src={women} alt='' />
+                </a> */}
+                <div
+                  className='item-collection-title'
+                  style={{ backgroundImage: `url(${women})` }}
+                >
+                  <h3>hello</h3>
+                </div>
+              </div>
             </Grid>
-            <Grid item xs={6} sm={3}>
-              <h2>2</h2>
+            <Grid item md={6} lg={3}>
+              <Grid md={12}>
+                <div className='item-collection-content'>
+                  {/* <a href='#'>
+                    <img src={bag} alt='' />
+                  </a> */}
+                  <div className='item-collection-title'>
+                    <h3>hello</h3>
+                  </div>
+                </div>
+              </Grid>
+              <Grid md={12}>
+                <div className='item-collection-content'>
+                  {/* <a href='#'>
+                    <img src={shoes} alt='' />
+                  </a> */}
+                  <div className='item-collection-title'>
+                    <h3>hello</h3>
+                  </div>
+                </div>
+              </Grid>
             </Grid>
-            <Grid item xs={6} sm={3}>
-              <h2>2</h2>
+            <Grid item md={6} lg={3}>
+              <div className='item-collection-content'>
+                {/* <a href='#'>
+                  <img src={women} alt='' />
+                </a> */}
+                <div className='item-collection-title'>
+                  <h3>hello</h3>
+                </div>
+              </div>
             </Grid>
           </Grid>
         </div>
