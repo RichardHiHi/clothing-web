f3b5a6ebd81c88e40aa695955203adc5fd3e0f03
thanh <thanhtrong.nguyen1602@gmail.com>
2021-04-04 01:30:49 +0700

    done section-title

--- src/components/blog-instagram/BlogInsta.js
+++ src/components/blog-instagram/BlogInsta.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './blogInsta.scss';
 const BlogInsta = () => {
   return (
-   <>
-    <div className='blog-section'>
-     <div className="blog-container">
-      <div className="section-title">
-       
+    <>
+      <div className='blog-section'>
+        <div className='blog-container section-container'>
+          <div className='section-title-container'>
+            <h3>
+              <span>LATES FROM BLOG</span>
+            </h3>
+            <span>The freshest and most exciting news</span>
+          </div>
+        </div>
       </div>
-     </div>
-    </div>
+      {/* <div className='insta-section'>
+        <div className='insta-container .section-container'>
+          <div className='section-title'></div>
+        </div>
+      </div> */}
     </>
   );
 };
