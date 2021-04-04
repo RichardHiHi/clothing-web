4d183e850042e72436893506e2066e6eddfd606b
thanh <thantrong.nguyen1602.com>
2021-04-04 12:07:33 +0700

    fix conflict

--- src/components/blog-instagram/BlogInsta.js
+++ src/components/blog-instagram/BlogInsta.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './blogInsta.scss';
 const BlogInsta = () => {
   return (
-   <>
-    <div className='blog-section'>
-     <div className="blog-container">
-      <div className="section-title">
-       
+    <>
+      <div className='blog-section'>
+        <div className='blog-container'>
+          <div className='section-title'>
+            
+          </div>
+        </div>
       </div>
-     </div>
-    </div>
     </>
   );
 };
