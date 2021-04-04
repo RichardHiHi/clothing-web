cbcd9ef37f87d2c9c8ef21722a592adceeb9312d
thanh <thanhtrong.nguyen1602@gmail.com>
2021-04-04 12:04:03 +0700

    change pc

--- src/components/blog-instagram/BlogInsta.js
+++ src/components/blog-instagram/BlogInsta.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './blogInsta.scss';
+import Slider from 'react-slick';
 const BlogInsta = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <>
       <div className='blog-section'>
@@ -11,6 +19,27 @@ const BlogInsta = () => {
             </h3>
             <span>The freshest and most exciting news</span>
           </div>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+          );
         </div>
       </div>
       {/* <div className='insta-section'>
